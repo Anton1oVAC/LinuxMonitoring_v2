2aadd9d1c85bba01843bdba28ea47d276ddc3872
Anton1o Vakhnin <anton1ovakhnin@MacBook-Air-Anton1o.local>
2024-08-23 12:55:30 +0300

    Changing the script due to errors

--- src/part01/check.sh
+++ src/part01/check.sh
@@ -1,65 +1,44 @@
 #!/bin/bash
 
-path_check='^/[a-zA-Z0-9/_-]+$'
-check_abs_path='^/$'
+free_memory_kb=$(df / | awk 'NR==2{print $4}')
+free_memory_mb=$(echo "$free_memory_kb 1024" | awk '{printf "%.0f", $1 / $2}')
 
-check_count_dir='^[1-9]+$' 
-check_letter_dir='^[a-zA-Z]{1,7}$'
-check_count_file='^[0-9]+$'
-check_letter_file='^[a-zA-Z]{1,7}\.{1,1}[a-zA-Z]{1,3}$'
+error_target=0
+NUMBER='[^0-9]+'
+ALPHABET='[^a-zA-Z]+'
 
-size_files='^[0-9]+(kb|KB|kB|Kb)$'
-size_number=$(echo "$6" | sed 's/[kK][bB]//')
+# Parameter
+path=$1
+number_of_folders=$2
+dir_name=$3
+number_of_files=$4
+file_name=$5
+size_file=$6
 
-check() {
-	if [ $count_param -ne 6 ]; then
-		echo -e "${RED}ERROR${NC}: invalid number of arguments!"
-		exit 1
 
-	elif ! [[ $1 =~ $path_check || $1 =~ $check_abs_path ]]; then
-		echo -e "${RED}ERROR${NC}: the first parameer is equal to the absolute path"
-		exit 2
+# Echo memory: mb
+echo "${GREEN}free memory mb${NC}: $free_memort_mb"
 
-	# Является ли параметр директорией
-	elif ! [[ -d $1 ]]; then
-		echo -e "${RED}ERROR${NC}: invalid directory"
-		exit 3
 
-	# Имеет ли объект право на запись
-	elif ! [[ -w $1 ]]; then
-		echo -e "${RED}ERROR${NC}: the object does not have write permissions"
-		exit 4
-	
-	elif ! [[ $2 =~ $check_count_dir ]]; then
-		echo -e "${RED}ERROR${NC}: invalid second parameter. ${RED}Number${NC}"
-		exit 5
+# Checking for six parameters
+if [ "$#" -ne 6 ]; then
+	echo -e "${RED}ERROR${NC}: The number of arguments should be 6"
+	exit 1
+fi
+avail_size=$(df -k / | grep /dev/mapper/ | awk '{print $4}')
+if [ $avail_size -le 1048576 ]; them
+	echo -e "${RED}ERROR${NC}: Not enough memory"
+	echo "Memory $avail_size"
+	exit 1
+fi
 
-	elif ! [[ $3 =~ $check_letter_dir ]]; then
-		echo -e "${RED}ERROR${NC}: invalid third parameter. ${RED}Letter${NC}"
-		exit 6
 
-	elif ! [[ $4 =~ $check_count_file ]]; then
-		echo -e "${RED}ERROR${NC}: invalid fourth parameter. ${RED}Number${NC}"
-		exit 7
+# First parameter
+if [[ ${1:$((${#1}-1)):1} != / ]]; then
+	echo -e "${RED}ERROR${NC}: The path must end with a '/'."
+	error_target=1
+elif [ ! -d $path ]; then
+	echo "${RED}ERROR${NC}: Folder $path doesn't exsist."
+	error_target=1
+fi
 
-	elif ! [[ $5 =~ $check_letter_file ]]; then
-		echo -e "${RED}ERROR${NC}: ivalid fifth parameter." 
-		echo -e "${GREEN}Two numbers${NC}:" 
-		echo -e "${GREEN}First digit of file name. Letters (Quantity 1-7)${NC}"
-		echo -e "${GREEN}Second digit of file extension. Letters (Quantity 1-3)${NC}"
-		exit 8
-
-	# костыль
-	elif ! [[ $6 =~ $size_files ]]; then
-		exit 9
-		
-	# Точнее, вот костыль
-	elif ! [[ $size_number =~ ^[0-9]+$ ]]; then
-		echo -e "${RED}ERROR${NC}: invalid sixth parameter. ${RED}Number+kb|KB|kB|Kb${NC}"
-    	exit 10
-
-	elif [ $size_number -gt 100 ]; then
-		echo -e "${RED}ERROR${NC}: large file"
-		exit 11
-	fi
-}
