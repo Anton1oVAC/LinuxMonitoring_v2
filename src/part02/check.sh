5d1b64644d1050ff9e87043dcb33feb58caef03a
Anton1o Vakhnin <anton1ovakhnin@MacBook-Air-Anton1o.local>
2024-08-30 12:23:58 +0300

    Ready script for creating a random number of folders and files, a random name and quantity

--- src/part02/check.sh
+++ src/part02/check.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+NC='\033[0m'
+RED='\033[031m'
+GREEN='\033[032m'
+
 error_target=0
 ALPHABET='[^a-zA-Z]+'
 NUMBER='[^0-9]+'
 
 start_time_nanosec=$(date +%s%N)
 start_time=$(date +%H:%M:%S)
+
+end_time_nanosec=$(date +%s%N)
+end_time=$(date +%H:%M:%S)
 
 # Parameter 
 create_of_folders=$1
@@ -22,40 +29,41 @@ avail_size=$(df -k / | grep /dev/mapper/ | awk '{print $4}')
 	echo -e "${GREEN}Free memory${NC}: $avail_size"
 if [ $avail_size -le 1048576 ]; then
 	echo -e "${RED}ERROR${NC}: Not enough memory"
+	exit 1
 fi
 
 
 # First parameter
-no_repeat_cheaker_dir=$(echo $create_of_files | sed  's/\(.\)\1/\1/g')
-if [ ${#create_of_files} -gt 7 ]; then
-	if [[ $error_target == 1 ]]; then 
-		echo
-	fi 
-	echo -e "${RED}ERROR${NC}: The number of characters to genetare a folder name should not exceed 7."
-	echo "Erroneous argument №1: $create_of_folders"
-	error_target=1
-elif [[ $create_of_folders =~ $ALPHABET ]]; then 
+no_repeat_cheker_dir=$(echo $create_of_folders | sed 's/\(.\)\1/\1/g')
+if [ ${#create_of_folders} -gt 7 ]; then
 	if [[ $error_target == 1 ]]; then
-		echo
-	fi
-	echo -e "${RED}ERROR${NC}: Only latin latters should be used to generate the folder name."
-	echo "Erroneous argument №1: $create_of_folders"
-	error_target=1
-elif [[ ${#no_repeat_cheaker_dir} -ne ${#create_of_files} ]]; then 
-	if [[ $error_target == 1 ]]; then 
 		echo 
-	fi 
-	echo -e "${RED}ERROR${NC}: The generating folder names should not be repeate."
-	echo "Erroneous argument №1: $create_of_folders"
+	fi
+	echo -e "${RED}ERROR${NC}: The number of characters to genetare a folder name should not exceed 7."
+	echo " Erroneous argument №1: $create_of_folders"
+	error_target=1
+elif [[ $create_of_folders =~ $ALPHABET ]]; then
+	if [[ $error_target == 1 ]]; then
+		echo 
+	fi	
+	echo -e "${RED}ERROR${NC}: Only latin latters should be used to generate the folder name"
+	echo " Erroneous argument №1: $create_of_folders"
+	error_target=1
+elif [[ ${#no_repeat_cheker_dir} -ne ${#create_of_folders} ]]; then
+	if [[ $error_target == 1 ]]; then
+		echo 
+	fi
+	echo -e "${RED}ERROR${NC}: The generating folder names should not be repeated"
+	echo " Erroneus argument №1: $create_of_folders "
 	error_target=1
 fi
 
 
 # Second marameter 
-symbol_for_file_name=${create_of_file%.*}
+symbol_for_file_name=${create_of_files%.*}
 symbol_for_expansion_file=${create_of_files#*.}
-no_repeat_cheaker_file_name=$(echo $create_of_files | sed 's/\(.\)\1/\1/g')
-no_repeat_cheaker_file_expansion=$(echo $create_of_files | sed 's/\(.\)\1/\1/g')
+no_repeat_cheaker_file_name=$(echo $symbol_for_file_name | sed 's/\(.\)\1/\1/g')
+no_repeat_cheaker_file_expansion=$(echo $symbol_for_expansion_file | sed 's/\(.\)\1/\1/g')
 if [[ ! $create_of_files == *.* ]]; then
 	if [[ $error_target == 1 ]]; then
 		echo
@@ -94,12 +102,12 @@ else
 		echo -e "${RED}ERROR${NC}: Only latin latters should be used to generate the folder name."
 		echo "Erroneous argument №2: $create_of_files"
 		error_target=1
-	elif [[ ${#no_repeat_cheaker_file_expansion} -ne ${#no_repeat_cheaker_file_name} ]]; then
+	elif [[ ${#no_repeat_cheaker_file_expansion} -ne ${#symbol_for_expansion_file} ]]; then
 		if [[ $error_target == 1 ]]; then
 			echo 
 		fi 
 		echo -e "${RED}ERROR${NC}: The generating folder names should not be repeate."
-		echo "Erroneous argument №1: $create_of_folders"
+		echo "Erroneous argument №2: $create_of_files"
 		error_target=1
 	fi
 fi
@@ -114,7 +122,7 @@ if [[ $size_num_files =~ $NUMBER && ! $size_file == *Mb ]]; then
 	echo -e "${RED}ERROR${NC}: The file size chould be described by the numnber and signature Mb after."
 	echo " Erroneus argument №3: $size_file"
 	error_target=1
-elif [[ $size_num_files -gt 100 ]]; then
+elif [ $size_num_files -gt 100 ]; then
 	if [[ $error_target == 1 ]]; then
 		echo 
 	fi
@@ -124,5 +132,38 @@ elif [[ $size_num_files -gt 100 ]]; then
 fi
 
 
-# # 
-# if [ $error_target != 1 ]; then
+#  creating folders and files
+if [[ $error_target != 1 ]]; then
+    touch "file.log"
+    data="_$(date +"%d%m%y")"
+    rand_folders=$(($RANDOM%100))
+    for (( i=1; i<=${rand_folders}; i++ )) do
+        avail_size=$(df -k / | grep /dev/mapper/ | awk '{print $4}')
+        	if [ $avail_size -le 1048576 ]; then
+            	echo -e "${RED}Stop${NC}: ${GREEN}Not enough memory for folders${NC}"
+            	break
+        	fi
+        path="$(find /home -type d 2>/dev/null | grep -E '[^(bin|sbin)]' | head -n$(($RANDOM%650)) | tail -n1)/"
+        name_dir="$(bash create.sh $create_of_folders $i)"
+        mkdir "${path}${name_dir}${data}"
+        echo -e "${path}${name_dir}${data}/\t\t\t\t\t$(date +"%d.%m.%y")" >> file.log
+        rand_files=$(($RANDOM%100))
+        for (( j=1; j<=${rand_files}; j++ )) do
+            avail_size=$(df -k / | grep /dev/mapper/ | awk '{print $4}')
+            	if [ $avail_size -le 1048576 ]; then
+                	echo -e "${RED}Stop${NC}: ${GREEN}Not enough memory for folders${NC}"
+                	break
+            	fi
+            name_file="$(bash create.sh $symbol_for_file_name $((j)))"
+            fallocate -l ${size_file} "${path}${name_dir}${data}/${name_file}${data}.${symbol_for_expansion_file}"
+            echo -e "${path}${name_dir}${data}/${name_file}${data}.${symbol_for_expansion_file}\t$(/bin/date +"%d.%m.%y") ${size_num_files}" >> file.log
+        done
+    done
+	echo -e "${GREEN}Free memory${NC}: $avail_size"
+    echo -e "${GREEN}Start time${NC}: $start_time"
+    echo -e "${GREEN}End time${NC}: $end_time"
+    echo -e "${GREEN}Total workong time${NC}: $((($end_time_nanosec - $start_time_nanosec)/1000000000)) seconds"
+    echo "Start time: $start_time" >> file.log
+    echo "End time: $end_time" >> file.log
+    echo "Total workong time$: $((($end_time_nanosec - $start_time_nanosec)/1000000000)) seconds" >> file.log
+fi
---
+++ src/part02/create.sh
@@ -0,0 +1,167 @@
+#!/bin/bash
+
+name_sym=$1
+size_sym=${#name_sym}
+caunter_generate=$2
+
+count_sym_1=1
+count_sym_2=1
+count_sym_3=1
+count_sym_4=1
+count_sym_5=1
+count_sym_6=1
+count_sym_7=1
+
+
+if [[ $size_sym -eq 1 ]]; then
+    name=$name_sym
+    for (( i=-2; i<$caunter_generate; i++ )) 
+    do
+        name+=$name_sym
+    done
+elif [[ $size_sym -eq 2 ]]; then
+    count_sym_1=2
+    count_sym_2=3
+    for (( i=1; i<$caunter_generate; i++ )) 
+    do
+        if [ $count_sym_1 -gt $count_sym_2 ]; then
+            let count_sym_2++
+            count_sym_1=1
+        else
+            let count_sym_1++
+        fi
+    done
+    for (( i=0; i<$( expr $count_sym_1 + $count_sym_2 ); i++ )) 
+    do
+        if [ $i -lt $count_sym_1 ]; then
+            name+="${name_sym:0:1}"
+        else
+            name+="${name_sym:1:1}"
+        fi
+    done
+elif [[ $size_sym -eq 3 ]]; then
+    count_sym_1=2
+    count_sym_3=2
+    for (( i=1; i<$caunter_generate; i++ )) 
+    do
+        if [ $count_sym_2 -gt $count_sym_3 ]; then
+            let count_sym_3++
+            count_sym_2=1
+            count_sym_1=1
+        elif [ $count_sym_1 -gt $count_sym_2 ]; then
+            let count_sym_2++
+            count_sym_1=1
+        else
+            let count_sym_1++
+        fi
+    done
+    for (( i=0; i<$( expr $count_sym_1 + $count_sym_2 + $count_sym_3 ); i++ )) 
+    do
+        if [ $i -lt $count_sym_1 ]; then
+            name+="${name_sym:0:1}"
+        elif [ $i -lt $( expr $count_sym_1 + $count_sym_2 ) ]; then
+            name+="${name_sym:1:1}"
+        else
+            name+="${name_sym:2:1}"
+        fi
+    done
+elif [[ $size_sym -eq 4 ]]; then
+    count_sym_1=2
+    for (( i=1; i<$caunter_generate; i++ )) 
+    do
+        if [ $count_sym_3 -gt $count_sym_4 ]; then
+            let count_sym_4++
+            count_sym_3=1
+            count_sym_2=1
+            count_sym_1=1
+        elif [ $count_sym_2 -gt $count_sym_3 ]; then
+            let count_sym_3++
+            count_sym_2=1
+            count_sym_1=1
+        elif [ $count_sym_1 -gt $count_sym_2 ]; then
+            let count_sym_2++
+            count_sym_1=1
+        else
+            let count_sym_1++
+        fi
+    done
+    for (( i=0; i<$( expr $count_sym_1 + $count_sym_2 + $count_sym_3 + $count_sym_4 ); i++ )) 
+    do
+        if [ $i -lt $count_sym_1 ]; then
+            name+="${name_sym:0:1}"
+        elif [ $i -lt $( expr $count_sym_1 + $count_sym_2 ) ]; then
+            name+="${name_sym:1:1}"
+        elif [ $i -lt $( expr $count_sym_1 + $count_sym_2 + $count_sym_3 ) ]; then
+            name+="${name_sym:2:1}"
+        else
+            name+="${name_sym:3:1}"
+        fi
+    done
+else
+    if [ $size_sym -eq 5 ]; then
+        count_sym_6=0
+        count_sym_7=0
+    elif [ $size_sym -eq 6 ]; then
+        count_sym_7=0
+    fi
+    for (( i=1; i<$caunter_generate; i++ )) 
+    do
+        if [ $size_sym -eq 7 ] && [ $count_sym_6 -gt $count_sym_7 ]; then
+            let count_sym_7++
+            count_sym_6=1
+            count_sym_5=1
+            count_sym_4=1
+            count_sym_3=1
+            count_sym_2=1
+            count_sym_1=1
+        elif [ $size_sym -gt 5 ] && [ $count_sym_5 -gt $count_sym_6 ]; then
+            let count_sym_6++
+            count_sym_5=1
+            count_sym_4=1
+            count_sym_3=1
+            count_sym_2=1
+            count_sym_1=1
+        elif [ $count_sym_4 -gt $count_sym_5 ]; then
+            let count_sym_5++
+            count_sym_4=1
+            count_sym_3=1
+            count_sym_2=1
+            count_sym_1=1
+        elif [ $count_sym_3 -gt $count_sym_4 ]; then
+            let count_sym_4++
+            count_sym_3=1
+            count_sym_2=1
+            count_sym_1=1
+        elif [ $count_sym_2 -gt $count_sym_3 ]; then
+            let count_sym_3++
+            count_sym_2=1
+            count_sym_1=1
+        elif [ $count_sym_1 -gt $count_sym_2 ]; then
+            let count_sym_2++
+            count_sym_1=1
+        else
+            let count_sym_1++
+        fi
+    done
+    for (( i=0; i<$( expr $count_sym_1 + $count_sym_2 + $count_sym_3 + $count_sym_4 + $count_sym_5 + $count_sym_6 + $count_sym_7); i++ )) 
+    do
+        if [ $i -lt $count_sym_1 ]; then
+            name+="${name_sym:0:1}"
+        elif [ $i -lt $( expr $count_sym_1 + $count_sym_2 ) ]; then
+            name+="${name_sym:1:1}"
+        elif [ $i -lt $( expr $count_sym_1 + $count_sym_2 + $count_sym_3 ) ]; then
+            name+="${name_sym:2:1}"
+        elif [ $i -lt $( expr $count_sym_1 + $count_sym_2 + $count_sym_3 + $count_sym_4 ) ]; then
+            name+="${name_sym:3:1}"
+        elif [ $i -lt $( expr $count_sym_1 + $count_sym_2 \
+             + $count_sym_3 + $count_sym_4 + $count_sym_5 ) ]; then
+            name+="${name_sym:4:1}"
+        elif [ $size_sym -gt 5 ] && [ $i -lt $( expr $count_sym_1 + $count_sym_2 \
+             + $count_sym_3 + $count_sym_4 + $count_sym_5 + $count_sym_6 ) ]; then
+            name+="${name_sym:5:1}"
+        elif [ $size_sym -eq 7 ]; then
+            name+="${name_sym:6:1}"
+        fi
+    done
+fi
+echo $name
